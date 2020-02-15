8bbdd269c4801a92f53cbacd70ab749484b3a78b
RSand18 <43037672+RSand18@users.noreply.github.com>
2020-02-15 08:03:20 -0600

    Update Elevator.c

--- Elevator.c
+++ Elevator.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define NUMFLOORS	15		//How many floors there are in the building
 
 
 int floorrange[15] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 }; // The array of floors the elevator can access
@@ -31,4 +30,5 @@ for (int i; i <= currentfloor; i++)
 int main()
 {
 	printf("What floor would you like to go to? The current floor is %d, and the max floor is 15.", currentfloor);
+	scanf(playeranswer);
 }
