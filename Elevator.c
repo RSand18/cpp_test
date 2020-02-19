9e6f7f8e2146eda105b3ded59f330e97072f86b5
RSand18 <43037672+RSand18@users.noreply.github.com>
2020-02-18 18:19:32 -0600

    Update Elevator.c

--- Elevator.c
+++ Elevator.c
@@ -4,7 +4,7 @@
 
 int floorrange[15] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 }; // The array of floors the elevator can access
 int currentfloor;	//The floor the player is currently on
-char playeranswer;
+int playerfloor;
 char flooryorn[] = "Would you like to go to a floor?%nPlease type 'y' for yes or 'n' for no.";	//Asks the player if they would like to move floors
 
 void changeFloor()
@@ -19,7 +19,9 @@ void changeFloor()
 int main()
 {
 	printf("What floor would you like to go to? The current floor is %d, and the max floor is 15.", currentfloor);
-	scanf("%d", &playeranswer);	//Player input
+	scanf("%d", &playerfloor);	//Player input
+	playerfloor = currentfloor;
+	changeFloor();
 	
 	
 	return 0;
